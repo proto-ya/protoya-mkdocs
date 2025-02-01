df26be01cb39121c1e1c3cb5f87e96818f2e0011
Yasuhisa <yhironaka@proto-ya.com>
2025-02-01 18:17:29 +0900

    OGPサーバー追加

--- docs/javascripts/ogpFetcher.js
+++ docs/javascripts/ogpFetcher.js
@@ -1,5 +1,5 @@
 async function fetchOGP(url, targetElementId) {
-    const apiUrl = `https://api.microlink.io/?url=${encodeURIComponent(url)}`;
+    const apiUrl = `http://localhost:3000/ogp?url=${encodeURIComponent(url)}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
